ed4db8103df66ad319d4bab1bdd44deca56baaad
udeyubi <a09584302@gmail.com>
2022-03-18 17:46:02 +0800

    feat 更新FB分享按鈕圖片及描述

--- resources/views/shop/layouts.blade.php
+++ resources/views/shop/layouts.blade.php
@@ -6,9 +6,9 @@
     <link href="https://fonts.googleapis.com/css2?family=Shadows+Into+Light&display=swap" rel="stylesheet">
     <meta property="og:url"                content="https://groulox.com/shop" />
     <meta property="og:type"               content="website" />
-    <meta property="og:title"              content="groulox" />
-    <meta property="og:description"        content="grouloxTest1657" />
-    <meta property="og:image"              content="http://static01.nyt.com/images/2015/02/19/arts/international/19iht-btnumbers19A/19iht-btnumbers19A-facebookJumbo-v2.jpg" />
+    <meta property="og:title"              content="buygoods!" />
+    <meta property="og:description"        content="最便宜和最貴的商品，在這裡都找不到!" />
+    <meta property="og:image"              content="{{ URL::asset('img/c628e0ac318d13760cbb76efa78d56c6.png') }}" />
 @endsection
 
 @section('content')
