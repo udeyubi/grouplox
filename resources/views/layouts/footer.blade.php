874baefadc840effd75d6e5769c99b0fe512aba9
udeyubi <a09584302@gmail.com>
2022-03-18 14:52:50 +0800

    style 更新footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,9 +1,11 @@
-<div class="row justify-content-around align-items-center w-100 bg-light py-3">
+<div class="row justify-content-around align-items-center w-100 bg-light py-3 text-center">
     <div class="col-3">
-        <i class="bi bi-facebook mx-3" style="font-size: 2rem; color: gray;"></i>
-        <i class="bi bi-twitter mx-3" style="font-size: 2rem; color: gray;"></i>
-        <i class="bi bi-youtube mx-3" style="font-size: 2rem; color: gray;"></i>
-        <i class="bi bi-instagram mx-3" style="font-size: 2rem; color: gray;"></i>
+        <a href="https://www.facebook.com/Groulox/" target="_blank"><i class="bi bi-facebook mx-3" style="font-size: 2rem; color: gray;"></i></a>
+        <a href="#"><i class="bi bi-twitter mx-3" style="font-size: 2rem; color: gray;"></i></a>
+        <a href="#"><i class="bi bi-youtube mx-3" style="font-size: 2rem; color: gray;"></i></a>
+        <a href="#"><i class="bi bi-instagram mx-3" style="font-size: 2rem; color: gray;"></i></a>
+
+        <span class="text-muted">customer.service@groulox.com</span>
     </div>
     <div class="col-3">
         <div class="fb-page" data-href="https://www.facebook.com/Groulox-101696569156698" data-tabs="" data-width="" data-height="" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/Groulox-101696569156698" class="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/Groulox-101696569156698">Groulox</a></blockquote></div>
